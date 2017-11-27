040efbc579d50612920cea7f3fdd721c5962ad6c
Tauhid Al Mahfuz Tusar <tauhidalmahfuz@gmail.com>
2017-11-27 02:58:03 +0000

    assignment 12

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+
+/*app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});*/
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -24,18 +18,14 @@ io.on('connection', function(socket){
   });
 });
 
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
 
-io.emit('some event', { for: 'everyone' });
-
-http.listen(port, process.env.IP, function(){
-  console.log('listening on *:' + port);
+http.listen(process.env.PORT, function(){
+  console.log('listening on *:'+process.env.PORT);
 });
